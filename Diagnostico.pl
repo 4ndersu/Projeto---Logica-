51607bb55acc084f685ca08833fa834fa8bc510a
4ndersu <anderson.vicente@discente.univasf.edu.br>
2025-11-11 21:00:05 -0300

    começando

--- Diagnostico.pl
+++ Diagnostico.pl
@@ -1,1 +1,10 @@
-problema(telaazul).
+componente(cpu).
+componente(memoriarRam).
+componente(placaMae).
+componente(armazenamento).
+componente(fonte).
+componente(placaDeVideo).
+componente(bateria).
+
+problema(naoliga) :-componente(memoriarRam); componente(fonte); componente(placaDeVideo).
+problema(travando) :-componente(memoriarRam); componente(cpu).
